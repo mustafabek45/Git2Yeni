b5b88711eb648464fffd4c31da456d0b4916cdb3
Mustafa Bek <bekmustafa45@gmail.com>
2022-08-15 21:50:37 +0200

    B1

--- src/Day2Git.java
+++ src/Day2Git.java
@@ -2,5 +2,8 @@ public class Day2Git {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Hello2");
+        System.out.println("Hello branch");
+
+
     }
 }
